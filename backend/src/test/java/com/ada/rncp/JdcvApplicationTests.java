a874ad02223cc39af3e6b46af9765973c30c7736
Sonia CHELLABI <chellabisonia@gmail.com>
2025-03-11 15:19:39 +0100

    Set up CI workflow with GitHub Actions

--- backend/src/test/java/com/ada/rncp/JdcvApplicationTests.java
+++ backend/src/test/java/com/ada/rncp/JdcvApplicationTests.java
@@ -3,7 +3,7 @@ package com.ada.rncp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.config.name=application-test")
+@SpringBootTest
 class JdcvApplicationTests {
 
 	@Test
